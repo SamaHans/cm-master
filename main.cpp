6be697916d5059c7c8c2ee876ce60488ee991ce9
Haohan <suncakes@foxmail.com>
2021-11-26 10:46:40 +0800

    initialize

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
-#include "sort.h"
+#include <iostream>
 
 int main(int argc, char* argv[]) {
 	
-	int nums[5] = { 0, 2, 1, 4, -100 };
-	sort::bubbleSort(nums, 5);
-	for (int i = 0; i < 5; ++i)
-		std::cout << nums[i] << std::endl;
+	std::cout << "Hello world!" << std::endl;
 	return 0;
 
 }
--- sort.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-class sort
-{
-public:
-    static void bubbleSort(int* nums, int len);
-};
